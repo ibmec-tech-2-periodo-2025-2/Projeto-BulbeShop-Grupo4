04490a159f9598c74bb2c347b483edd40fdbc33e
Gabriel Montandon <gabrieldoornik@gmail.com>
2025-11-02 00:14:17 -0300

    fix: correção de aspas

--- produtos-data.js
+++ produtos-data.js
@@ -3,49 +3,7 @@ const PRODUTOS = [
         id: 1,
         nome: "Avant Neo LED Smart 10W RGB",
         descricao: "Lâmpada inteligente com controle via app Avant NEO, compatível com Alexa e Google Home. Permite criar cenários personalizados, agendamento e controle de intensidade. Ideal para ambientes internos como quartos, salas e escritórios.<br>• Potência: 10W<br> • Fluxo: 810 lumens <br> • Soquete: E27 <br> • Voltagem: Bivolt (100-240V) <br> • Conectividade: WiFi 2.4GHz <br> • Cores: 16 milhões (RGB)<br> • Temperatura: 2700K-6500K ",
-        preco: R$ 35,00 ,
-        avaliacao: (4.3/5),
-    }
-        {
-        id: ,
-        nome: ,
-        descricao: ,
-        preco: ,
-        avaliacao: ,
-    }
-        {
-        id: ,
-        nome: ,
-        descricao: ,
-        preco: ,
-        avaliacao: ,
-    }
-        {
-        id: ,
-        nome: ,
-        descricao: ,
-        preco: ,
-        avaliacao: ,
-    }
-        {
-        id: ,
-        nome: ,
-        descricao: ,
-        preco: ,
-        avaliacao: ,
-    }
-        {
-        id: ,
-        nome: ,
-        descricao: ,
-        preco: ,
-        avaliacao: ,
-    }
-        {
-        id: ,
-        nome: ,
-        descricao: ,
-        preco: ,
-        avaliacao: ,
+        preco: "R$ 35,00",
+        avaliacao: "(4.3/5)",
     }
 ]
