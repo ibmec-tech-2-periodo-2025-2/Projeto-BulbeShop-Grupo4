5877812d99920a22e86fac16bc8aeebc64e9fca3
João Pedro Lodi <joaoplfranca@gmail.com>
2025-11-11 17:54:41 -0300

    fix: melhoria no complemento

--- ENDEREÇO/endereco.js
+++ ENDEREÇO/endereco.js
@@ -4,6 +4,7 @@ const endereco = document.getElementById("ender");
 const numero = document.getElementById("num");
 const complemento = document.getElementById("comp");
 
+// Salvar dados
 botao.addEventListener("click", (event) => {
   event.preventDefault();
 
@@ -15,10 +16,11 @@ botao.addEventListener("click", (event) => {
   window.location.href = "../PAGAMENTO/pagamento.html";
 });
 
-const inputs = [cep, endereco, numero, complemento];
+// Ativar botão
+const inputsobg = [cep, endereco, numero];
 
 function verificarCampos() {
-  const todosPreenchidos = inputs.every(input => input.value.trim() !== '');
+  const todosPreenchidos = inputsobg.every(input => input.value.trim() !== '');
 
   if (todosPreenchidos) {
     botao.classList.remove('desabilitado');
@@ -27,8 +29,17 @@ function verificarCampos() {
   }
 }
 
-inputs.forEach(input => {
+inputsobg.forEach(input => {
   input.addEventListener('input', verificarCampos);
 });
 
 verificarCampos();
+
+// Melhoria ao digitar CEP
+cep.addEventListener("input", function () {
+  this.value = this.value.replace(/\D/g, "");
+  this.value = this.value.replace(/^(\d{5})(\d)/, "$1-$2");
+  if (this.value.length > 9) {
+    this.value = this.value.slice(0, 9);
+  }
+});
