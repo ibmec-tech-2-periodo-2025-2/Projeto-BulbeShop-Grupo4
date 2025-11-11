d5bee1cd98d167c8923a33fd95d9e6de9d1dc2e2
Henrique Pinheiro <henriquepinharaujo@gmail.com>
2025-11-11 20:38:49 -0300

    feat:funcionalidade coracao colorido e link funcional

--- PRODUTO/produto.js
+++ PRODUTO/produto.js
@@ -28,3 +28,25 @@ function mostrarImagem(novoIndice) {
 
 anterior.addEventListener('click', () => mostrarImagem(indice - 1));
 proximo.addEventListener('click', () => mostrarImagem(indice + 1));
+
+const coracao = document.querySelector(".coracao");
+coracao.addEventListener("click", () => {
+  coracao.classList.toggle("ativo");
+})
+
+const botaoCompartilhar = document.querySelector(".link");
+
+botaoCompartilhar.addEventListener("click", () => {
+  const url = window.location.href; // pega o link da página atual
+
+  // tenta copiar o link para a área de transferência
+  navigator.clipboard.writeText(url)
+    .then(() => {
+      alert("🔗 Link copiado para a área de transferência!");
+    })
+    .catch(err => {
+      console.error("Erro ao copiar link: ", err);
+      alert("❌ Não foi possível copiar o link.");
+    });
+});
+
