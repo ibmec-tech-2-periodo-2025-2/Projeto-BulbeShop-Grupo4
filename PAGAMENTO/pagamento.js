3b7fb230fa8426c4124d7dc9b1b965ccd3fe3553
Gabriel Fonseca <gabriel.gfonseca2007@gmail.com>
2025-11-18 19:13:00 -0300

    feat:cancelar envio padrao

--- PAGAMENTO/pagamento.js
+++ PAGAMENTO/pagamento.js
@@ -1,4 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
+  const cod_seg = document.getElementById('cod_seg');
+  const validade = document.getElementById('validade');
+  const num_cc = document.getElementById('num_cc');
+  const nome_cartao = document.getElementById('nome_cartao');
+  const confirmar = document.querySelector('.b_confirmar');
   
   const selectPagamento = document.getElementById("pagamento");
   const formCredito = document.querySelector(".credito");
@@ -7,10 +12,19 @@ document.addEventListener("DOMContentLoaded", function() {
     const valor = selectPagamento.value; 
 
     if (valor === "credito" || valor === "debito") {
-      formCredito.style.display = "flex"; 
+      formCredito.style.display = "flex";
     } else {
       formCredito.style.display = "none";
     }
   });
+  confirmar.addEventListener('click', (event) => {
+    console.log("Botão clicado!");
+    event.preventDefault();
+    if((cod_seg.value === '') || (validade.value === '') || (num_cc.value === '') || (nome_cartao.value === '')){
+      window.alert("Informações do cartão inválidas, digite novamente.");
+    }else{
+      window.location.href = "../CONFIRMACAO/TCC.html";
+    }
+  })
 
 });
